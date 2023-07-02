2e6b2c65ce3c39750912318ea42816db46a1b53b
Bhavesh Yadav <codezera3@gmail.com>
2023-07-02 20:09:27 +0530

    added feed component front end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import '@fontsource/roboto/300.css';
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import {Navbar, ChannelDetail, Feed, SearchFeed, VideoDetail} from './components'
 
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
-}
+const App = () => (
+
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed/>} />
+                <Route path='/video/:id' exact element={<VideoDetail/>} />
+                <Route path='/channel/:id' exact element={<ChannelDetail/>} />
+                <Route path='/search/:searchTerm' exact element={<SearchFeed/>} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+
+);
 
 export default App
